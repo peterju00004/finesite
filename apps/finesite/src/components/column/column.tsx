b371b43196fbc9b1a95d42f230926b4319acaf2a
Peter Ju <peterju00004@gmail.com>
2025-07-20 09:32:45 -0400

    create column

---
+++ apps/finesite/src/components/column/column.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './column.module.scss';
+
+interface ColumnItemProps {
+  slot?: string;
+  className?: string;
+}
+
+export function Column(props: React.PropsWithChildren<{}>) {
+  const getSlot = (slot: string) => {
+    return props.children && React.Children.map(props.children, (child) => {
+      if (React.isValidElement(child) && (child.props as ColumnItemProps)?.slot === slot) {
+        return React.cloneElement(child as React.ReactElement<ColumnItemProps>, {
+          className: `${(child.props as ColumnItemProps).className || ''} ${styles['column-link']}`
+        });
+      }
+      return null;
+    });
+  }
+
+  const items = getSlot('item');
+
+  return (
+    <div className={styles['column']}>
+      <nav className={styles['column-nav']}>
+        <ul className={`${styles['column-list']} ${styles['column-list-no-decoration']}`}>
+          {items && React.Children.map(items, (item) => {
+            if (React.isValidElement(item)) {
+              return (
+                <li key={item.key} className={styles['column-item']}>
+                  {item}
+                </li>
+              );
+            }
+            return null;
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Column;
